70a381f9379d844ca165175a8744563644c63d21
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-10-25 05:19:23 +0530

    A file which will contain all the Configurations related to fonts which project uses

---
+++ priedu-website/src/app/fonts/font.js
@@ -0,0 +1,12 @@
+import { Roboto_Mono, Lexend_Deca } from 'next/font/google'
+ export const font=Roboto_Mono({
+    subsets:['latin'],
+    weight:'600',
+    style:'normal'
+})
+
+export const myfont=Lexend_Deca({
+    subsets:['latin'],
+    weight:['200','400'],
+    style:'normal'
+})
