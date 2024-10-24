6cd9190642df72b0b50e3950abb64cbf5f23d581
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-10-25 05:22:03 +0530

    layout of the website with some alterations

--- priedu-website/src/app/layout.js
+++ priedu-website/src/app/layout.js
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from './component/header'
 
+import Footer from './component/footer'
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -23,7 +25,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
