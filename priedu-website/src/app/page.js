e1a62b952b4dd70ae748330e6502965e19f3b624
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-10-25 05:19:55 +0530

    Main page of the website

---
+++ priedu-website/src/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from '../app/home/page'
+import Program from '../app/programs/page'
+const page = () => {
+  return (
+    <>
+    <Home />
+    <Program />
+    </>
+  )
+}
+
+export default page
