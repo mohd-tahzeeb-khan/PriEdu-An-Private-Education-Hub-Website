84ce242c59f32037c95ab20b7f06b39766ffeef6
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-10-26 19:45:29 +0530

    working on Inspiration section

---
+++ priedu-website/src/app/inspiration/page.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const inpiration = () => {
+  return (
+    <>
+    <div className='grid-rows-3'>
+        <div className='bg-green-500 flex justify-around'>
+            <div>1</div>
+            <div>2</div>
+        </div>
+        <div className='bg-red-500 flex justify-around'>
+            <div>1</div>
+            <div>2</div>
+        </div>
+        <div className='bg-slate-500 flex justify-around'>
+            <div>1</div>
+            <div>2</div>
+        </div>
+
+
+    </div>
+    </>
+  )
+}
+
+export default inpiration
