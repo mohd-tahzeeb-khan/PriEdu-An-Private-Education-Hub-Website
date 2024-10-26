a525953c6fbbeef4934c53353a47bacc0ff14fdd
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-10-27 02:06:27 +0530

    ...

--- priedu-website/src/app/page.js
+++ priedu-website/src/app/page.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Home from '../app/home/page'
 import Program from '../app/programs/page'
 import Inspiration from '../app/inspiration/page'
+import History from '../app/history/page'
+import Contact from '../app/contact/page'
 const page = () => {
   return (
     <>
     <Home />
-    <Program />
     <Inspiration />
+    <History />
+    <Program />
+    <Contact />
     </>
   )
 }
