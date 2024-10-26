2805ac539dfb52d508206a124dd3c952774bac97
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-10-26 19:46:23 +0530

    Added the Section called --"Inpiration"

--- priedu-website/src/app/page.js
+++ priedu-website/src/app/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from '../app/home/page'
 import Program from '../app/programs/page'
+import Inspiration from '../app/inspiration/page'
 const page = () => {
   return (
     <>
     <Home />
     <Program />
+    <Inspiration />
     </>
   )
 }
